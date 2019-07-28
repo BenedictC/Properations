aa91fe4944d9a6d8e56a4d61d17970ecbf42bcaf
Benedict Cohen <ben@benedictcohen.co.uk>
2019-07-28 11:04:28 +0100

    Fixes import

--- Properations/Resources/Properations.h
+++ Properations/Resources/Properations.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Benedict Cohen. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Properations.
 FOUNDATION_EXPORT double ProperationsVersionNumber;
