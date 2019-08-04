9f5821fb915d6ee70f2320c95d0e5204ee25e18d
Benedict Cohen <ben@benedictcohen.co.uk>
2019-08-04 19:23:47 +0100

    Adds mapping tests

--- ProperationsTests/Extensions/Future+MappingTests.swift
+++ ProperationsTests/Extensions/Future+MappingTests.swift
@@ -66,44 +66,158 @@ class Future_MappingTests: XCTestCase {
     // MARK: compactMapElementsToValue
 
     func testCompactMapElementsToValueWithSuccess() throws {
-        XCTFail()
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = ["1", "2"]
+
+        let future = initial.compactMapElementsToValue { $0.map { "\($0)" } }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.successValue
+        XCTAssertEqual(actual, expected)
     }
 
     func testCompactMapElementsToValueWithAFailure() throws {
-        XCTFail()
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = TestError.error
+
+        let future = initial.compactMapElementsToValue { int -> String? in
+            guard let int = int else { return nil }
+            guard int == 1 else { throw TestError.error }
+            return "\(int)"
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.failureValue
+        XCTAssertEqual(equatableDescription(of: actual), equatableDescription(of: expected))
     }
 
 
     // MARK: compactMapElementsToFuture
 
     func testCompactMapElementsToFutureWithSuccess() throws {
-        XCTFail()
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = ["1", "2"]
+
+        let future = initial.compactMapElementsToFuture { int -> Future<String>? in
+            guard let int = int else { return nil }
+            return Promises.makeFulfilled(with: .success("\(int)"))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.successValue
+        XCTAssertEqual(actual, expected)
     }
 
-    func testCompactMapElementsToFutureAFailure() throws {
-        XCTFail()
+    func testCompactMapElementsToFutureWithAMapFailure() throws {
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = TestError.error
+
+        let future = initial.compactMapElementsToFuture { int -> Future<String>? in
+            guard let int = int else { return nil }
+            guard int == 1 else { throw TestError.error }
+            return Promises.makeFulfilled(with: .success("\(int)"))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.failureValue
+        XCTAssertEqual(equatableDescription(of: actual), equatableDescription(of: expected))
     }
 
+    func testCompactMapElementsToFutureWithAFutureFailure() throws {
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = ProperationsError.multipleErrors([nil, TestError.error])
+
+        let future = initial.compactMapElementsToFuture { int -> Future<String>? in
+            guard let int = int else { return nil }
+            if int == 1 {
+                return Promises.makeFulfilled(with: .success("\(int)"))
+            }
+            return Promises.makeFulfilled(with: .failure(TestError.error))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.failureValue
+        XCTAssertEqual(equatableDescription(of: actual), equatableDescription(of: expected))
+    }
 
     // MARK: mapElementsToValue
 
     func testMapElementsToValueWithSuccess() throws {
-        XCTFail()
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = ["1", "", "2"]
+
+        let future = initial.mapElementsToFuture { int -> Future<String> in
+            guard let int = int else { return Promises.makeFulfilled(with: .success("")) }
+            return Promises.makeFulfilled(with: .success("\(int)"))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.successValue
+        XCTAssertEqual(actual, expected)
     }
+
 
     func testMapElementsToValueWithAFailure() throws {
-        XCTFail()
-    }
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = TestError.error
 
+        let future = initial.mapElementsToValue { int -> String in
+            guard let int = int else { return "" }
+            guard int == 1 else { throw TestError.error }
+            return "\(int)"
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.failureValue
+        XCTAssertEqual(equatableDescription(of: actual), equatableDescription(of: expected))
+    }
 
     // MARK: mapElementsToFuture
 
-    func testElementsToFutureWithSuccess() throws {
-        XCTFail()
+    func testMapElementsToFutureWithSuccess() throws {
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = ["1", "", "2"]
+
+        let future = initial.mapElementsToFuture { int -> Future<String> in
+            guard let int = int else { return Promises.makeFulfilled(with: .success("")) }
+            return Promises.makeFulfilled(with: .success("\(int)"))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.successValue
+        XCTAssertEqual(actual, expected)
     }
 
-    func testElementsToFutureWithAFailure() throws {
-        XCTFail()
+    func testMapElementsToFutureWithAMapFailure() throws {
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = TestError.error
+
+        let future = initial.mapElementsToFuture { int -> Future<String> in
+            guard let int = int else { return Promises.makeFulfilled(with: .success("")) }
+            guard int == 1 else { throw TestError.error }
+            return Promises.makeFulfilled(with: .success("\(int)"))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.failureValue
+        XCTAssertEqual(equatableDescription(of: actual), equatableDescription(of: expected))
+    }
+
+    func testMapElementsToFutureWithAFutureFailure() throws {
+        let initial = makeAsynchronouslyFulfilledFuture(with: .success([1, nil, 2]))
+        let expected = ProperationsError.multipleErrors([nil, nil, TestError.error])
+
+        let future = initial.mapElementsToFuture { int -> Future<String> in
+            guard let int = int else { return Promises.makeFulfilled(with: .success("")) }
+            if int == 1 {
+                return Promises.makeFulfilled(with: .success("\(int)"))
+            }
+            return Promises.makeFulfilled(with: .failure(TestError.error))
+        }
+        try wait(forCompletionOf: future)
+
+        let actual = future.result?.failureValue
+        XCTAssertEqual(equatableDescription(of: actual), equatableDescription(of: expected))
     }
     
 
